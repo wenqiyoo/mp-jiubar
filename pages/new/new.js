e2b5e86fde8932617b9b8f1dc25e3ec7d5f3ef10
Ashkan Yadegari <ashkan.yadegari@outlook.com>
2020-11-25 14:16:34 +0800

    form fixed

--- pages/new/new.js
+++ pages/new/new.js
@@ -9,9 +9,48 @@ Page({
     pickerHidden: true,
     chosen: ''
   },
-  formSubmit(e) {
-    console.log('form发生了submit事件，携带数据为：', e.detail.value)
-    console.log(e)
+  formSubmit: function(event) {
+    console.log('form发生了submit事件，携带数据为：', event.detail.value)
+    console.log(event)
+    console.log("+++++")
+
+    let title = event.detail.value.title;
+    console.log(title)
+    let description = event.detail.value.description;
+    let price = event.detail.value.price;
+    let address = event.detail.value.address;
+    let user_id = getApp().globalData.user.id;
+
+    let catering = {
+      title: title,
+      description: description,
+      price: price,
+      address: address,
+      user_id: user_id
+    }
+
+    console.log("------")
+
+    console.log(catering)
+
+    console.log("------")
+
+
+    wx.request({
+      url: `http://localhost:3000/api/v1/caterings`,
+      method: 'POST',
+      data: catering,
+      success(res) {
+        // redirect to index page when done
+        console.log(res);
+        wx.showToast({
+          title: 'Testt',
+        })
+        wx.reLaunch({
+          url: '/pages/index/index'
+        });
+      }
+    });
   },
 
   formReset(e) {
